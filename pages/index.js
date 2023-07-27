0b0d0be5d86396812ea8abbdda3679094977a41b
Kurt Liang <liangke1991@gmail.com>
2023-07-27 14:29:50 +1200

    login authentication, configure mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <div className={'bg-red-500'}>test</div>
+  const { data: session } = useSession()
+  if (!session) {
+    return (
+      <div className={'bg-red-500 w-screen h-screen flex items-center'}>
+        <div className={`text-center w-full`}>
+          <button
+            onClick={() => signIn('google')}
+            className={`bg-white p-2 rounded-lg`}
+          >
+            Log in with Google
+          </button>
+        </div>
+      </div>
+    )
+  }
+  return <div>logged in {session.user.email}</div>
 }
+
+// export default function Component() {
+//   if(session) {
+//     return <>
+//       Signed in as {session.user.email} <br/>
+//       <button onClick={() => signOut()}>Sign out</button>
+//     </>
+//   }
+//   return <>
+//     Not signed in <br/>
+//     <button onClick={() => signIn()}>Sign in</button>
+//   </>
+// }
