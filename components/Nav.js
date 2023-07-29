f0bef5921dd95fdae4dbdc04048b8f44879c0799
Kurt Liang <liangke1991@gmail.com>
2023-07-30 10:51:36 +1200

    create paths for nav bar

--- components/Nav.js
+++ components/Nav.js
@@ -31,9 +31,7 @@ function Nav() {
       <nav className="flex flex-col gap-2">
         <Link
           href={'/dashboard'}
-          className={
-            pathname.includes('/dashboard') ? activeLink : inactiveLink
-          }
+          className={pathname.includes('/') ? activeLink : inactiveLink}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
