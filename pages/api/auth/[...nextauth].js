ae57b0c012a7f93e68ca066ea34e7abac4d0f670
Kurt Liang <liangke1991@gmail.com>
2023-07-30 10:50:41 +1200

    connect to the mongoDB

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,6 @@
 import clientPromise from '@/lib/mongodb'
 import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import NextAuth from 'next-auth'
-
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
