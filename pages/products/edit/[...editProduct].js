a4dbc603b66c3ead6ffbec2f1779961462f6d08a
Kurt Liang <liangke1991@gmail.com>
2023-08-03 17:43:03 +1200

    fetch data from DB and display on products page, create edit page to edit each product

---
+++ pages/products/edit/[...editProduct].js
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+function EditProductPage() {
+  return (
+    <Layout>
+      <div>edit product from here</div>
+    </Layout>
+  )
+}
+
+export default EditProductPage
