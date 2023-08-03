aa47add076bfea62f8ef836bb0570271cca0eb46
Kurt Liang <liangke1991@gmail.com>
2023-08-03 19:59:06 +1200

    productFrom sepretely put into new product and edit product create api function to update data

--- pages/api/products.js
+++ pages/api/products.js
@@ -5,7 +5,11 @@ export default async function handler(req, res) {
   const { method } = req
   await mongooseConnect()
   if (method === 'GET') {
-    res.json(await Product.find())
+    if (req.query?.id) {
+      res.json(await Product.findOne({ _id: req.query.id }))
+    } else {
+      res.json(await Product.find())
+    }
   }
   if (method === 'POST') {
     const { title, description, price } = req.body
@@ -16,7 +20,11 @@ export default async function handler(req, res) {
     })
     res.json(productDoc)
   }
-
+  if (method === 'PUT') {
+    const { title, description, price, _id } = req.body
+    await Product.updateOne({ _id }, { title, description, price })
+    res.json(true)
+  }
   // const { method } = req
   // if (method === 'POST') {
   //   try {
--- pages/products/new.js
+++ pages/products/new.js
@@ -3,80 +3,13 @@ import axios from 'axios'
 
 import { useRouter } from 'next/router'
 import { useState } from 'react'
-
+import ProductForm from '@/components/ProductForm'
 export default function NewProducts() {
-  const [title, steTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [price, setPrice] = useState('')
-  const [goToProduct, setGoToProduct] = useState(false)
-  const router = useRouter()
-
-  async function createProduct(ev) {
-    ev.preventDefault()
-    // try {
-    //   const response = await fetch('/api/products', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ title, description, price }),
-    //   })
-
-    //   if (response.ok) {
-    //     // Product created successfully
-    //     console.log('Product created successfully!')
-    //   } else {
-    //     // Handle error response
-    //     console.error('Error creating product:', response.statusText)
-    //   }
-    // } catch (error) {
-    //   // Handle network or other errors
-    //   console.error('Error creating product:', error.message)
-    // }
-
-    const data = { title, description, price }
-    try {
-      await axios.post('/api/products', data)
-      // Product created successfully
-      console.log('Product created successfully!')
-
-      router.push('/products') // Redirect to the product page after creating the product
-    } catch (error) {
-      // Handle error response
-      console.error('Error creating product:', error.message)
-    }
-  }
   return (
     <Layout>
-      <form onSubmit={createProduct}>
-        <h1>New Products</h1>
-        <label>Products Name</label>
+      <h1>New Products</h1>
 
-        <input
-          type="text"
-          placeholder="Add new products here"
-          value={title}
-          onChange={(ev) => steTitle(ev.target.value)}
-        />
-        <label>description</label>
-        <textarea
-          type="text"
-          placeholder="description"
-          value={description}
-          onChange={(ev) => setDescription(ev.target.value)}
-        />
-        <label>Price (in NZD)</label>
-
-        <input
-          type="number"
-          placeholder="price"
-          value={price}
-          onChange={(ev) => setPrice(ev.target.value)}
-        />
-        <button type="submit" className="btn-primary">
-          Save
-        </button>
-      </form>
+      <ProductForm />
     </Layout>
   )
 }
